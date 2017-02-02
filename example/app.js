349324c2ce9f1c697afe0178831178a8c1ba97f5
Amy Cheong <amy.cheong@tigerspike.com>
2017-02-02 15:33:22 +0800

    Added delete

--- example/app.js
+++ example/app.js
@@ -51,11 +51,11 @@ app.get('/', function(req, res){
 
 // User
 app.get('/users', user.list);
-app.all('/user/:id/:op?', user.load);
+// app.all('/user/:id/:op?', user.load);
 app.get('/user/:id', user.view);
 app.get('/user/:id/view', user.view);
 app.put('/user/:id/edit', user.update);
-
+app.delete('/users', user.delete);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
@@ -63,4 +63,4 @@ http.createServer(app).listen(app.get('port'), function(){
 
 //TODO: app.configure is deprecated - need to update to express4
 //Check if run different proj under 1 server port
-// Error handling 
+// Error handling
--- example/user-api.js
+++ example/user-api.js
@@ -19,9 +19,11 @@
  */
 var users = [
   { name: 'TJ', email: 'tj@vision-media.ca' },
-  { name: 'Tobi', email: 'tobi@vision-media.ca' }
+  { name: 'Tobi', email: 'tobi@vision-media.ca' },
+  { name: 'Tandrew', email: 'tobi@vision-media.ca' },
+  { name: 'Tom', email: 'tobi@vision-media.ca' },
+  { name: 'Stranger', email: 'tobi@vision-media.ca' }
 ];
-
 
 /**
  * @swagger
@@ -58,7 +60,7 @@ exports.list = function(req, res){
   *          dataType: integer
   */
 exports.view = function(req, res){
-  res.json(req.user)
+  res.json(users[req.params.id] || { error: 'Cannot find user' });
 };
 
 /**
@@ -90,9 +92,53 @@ exports.view = function(req, res){
  *          dataType: string
  */
 exports.update = function(req, res){
-  req.user.name = req.query.name;
-  req.user.email = req.query.email;
-  res.json(req.user)
+  var id = req.params.id;
+  req.user = users[id];
+  if (req.user) {
+    req.user = users[req.params.id]
+    req.user.name = req.query.name;
+    req.user.email = req.query.email;
+    res.json(req.user)
+  }
+  else {
+    res.status(404).json({ error: 'cannot find user ' + id });
+  }
+
+};
+
+/**
+ * @swagger
+ * path: /user/delete
+ * operations:
+ *   -  httpMethod: DELETE
+ *      summary: Delete user(s) with id array
+ *      notes: Returns success or fail on deletion
+ *      nickname: deleteUsers
+ *      produces:
+ *       - application/json
+ *      parameters:
+ *        - name: id
+ *          description: User ids
+ *          paramType: query
+ *          required: true
+ *          collectionFormat: multi
+ *          dataType: array
+ *          items:
+ *             dataType: string
+ */
+exports.delete = function(req, res){
+
+  var deletedIds = req.query.id
+
+  for (i in deletedIds) {
+      req.user = users[i]
+      console.log(req.user);
+      if(req.user){
+        users.splice(i, 1);
+      }
+  }
+
+  res.status(200).json(users);
 };
 
 //Private method to load all users internally before users/ api
