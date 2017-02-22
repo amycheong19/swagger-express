891e6825db1a075ead99cddaced8b94be7d53c55
Amy Cheong <amy.cheong@tigerspike.com>
2017-02-22 11:43:13 +0800

    Updated cors
    Signed-off-by: Amy Cheong <amy.cheong@tigerspike.com>

--- example/app.js
+++ example/app.js
@@ -46,9 +46,8 @@ app.get('/meetingRooms/swagger.json', function(req, res) {
 
 
 /**
- * Server & port setup
- two*/
- // serve swagger
+ * Server & port setup */
+ // Added header for CORS
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
